e4e514647e53133e78364676307ecd7ceb335c44
VALR <641749563@qq.com>
2019-06-05 13:47:17 +0800

    去除不必要的日志打印

--- src/middlewares/requestHandle/index.js
+++ src/middlewares/requestHandle/index.js
@@ -3,7 +3,7 @@
  * @LastEditors: yongtian.hong
  * @Description: 请求参数统一挂载至ctx.params
  * @Date: 2019-02-12 16:40:18
- * @LastEditTime: 2019-06-04 10:19:10
+ * @LastEditTime: 2019-06-05 13:10:42
  */
 module.exports = function () {
     return async (ctx, next) => {
@@ -13,7 +13,7 @@ module.exports = function () {
         } else if (["POST", "PUT"].includes(method)) {
             ctx.params = ctx.request.body;
         }
-        console.log('requestUrl', ctx.url)
+        // console.log('requestUrl', ctx.url)
         await next();
     };
 };
