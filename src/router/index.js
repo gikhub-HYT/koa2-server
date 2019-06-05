0f18721e27546e35046086e282c2b46ff51aa155
VALR <641749563@qq.com>
2019-06-05 13:09:17 +0800

    修复路由加载无法访问

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,11 @@ const path = require("path");
 const Router = require("koa-router");
 
 const router = new Router({
-  // prefix: "/api/"
+  prefix: "/api/"
 });
 
-
+// 允许使用的方法
 const allowMethods = ["put", "post", "delete", "get", "patch"];
-
 
 // 处理相对路径
 function resolve(dir) {
@@ -17,23 +16,23 @@ function resolve(dir) {
 
 //处理请求路径拼接
 function pathHandle(path) {
-  return path.startsWidth('/') ? path : `/${path}`
+  return path.startsWith('/') ? path : `/${path}`
 }
 
 // 配置控制器
 function setController(mapping, moduleName) {
 
-  let path = `${moduleName}${pathHandle(mapping["path"])}`;
-
-  console.error(`PATH: ${path}`);
+  let path = `${moduleName}${pathHandle(mapping["path"])}`.trim();
+  // console.error(`PATH: ${path}`);
   // let path = mapping["url"];
-  let method = mapping["method"].toLowerCase();
+  let method = mapping["method"].trim().toLowerCase();
 
   let controller = mapping["controller"];
 
   // 判断请求方式是否为允许方法
   if (allowMethods.includes(method)) {
     router[method](path, controller);
+    // console.log('router', router)
   } else if (!method) {
     console.error(`the field method in module ${moduleName} can not be empty!`);
   } else {
@@ -46,7 +45,7 @@ function routerMap(mappings, filename) {
   let keys = Object.keys(mappings);
   if (keys.includes("controller") && keys.includes("method")) {
     setController(mappings, filename);
-    console.error(`routerMap: ${mappings} `);
+    // console.error(`routerMap: ${mappings} `);
   } else {
     keys.forEach(key => {
       setController(mappings[key], filename);
@@ -71,8 +70,8 @@ function addControllers(targetPath) {
     });
     // 读取模块所有的控制器文件
     for (let js_file of controller_js_files) {
-      let mapping = require(`${path.resolve(modulePath, js_file)} `);
-      console.log("mapping", mapping);
+      let mapping = require(`${path.resolve(modulePath, js_file)}`);
+      // console.log("mapping", mapping);
       routerMap(mapping, module_name);
     }
   })
