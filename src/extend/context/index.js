3c3bd55efd602b037c5bd55c715110963de87f2d
VALR <641749563@qq.com>
2019-07-20 20:18:44 +0800

    修改模块读取扩展

--- src/extend/context/index.js
+++ src/extend/context/index.js
@@ -2,7 +2,7 @@
  * @Author: yongtian.hong
  * @Date: 2019-02-09 21:06:21
  * @LastEditors: yongtian.hong
- * @LastEditTime: 2019-04-07 16:06:24
+ * @LastEditTime: 2019-06-05 21:01:54
  * @Description: ctx对象拓展
  */
 
@@ -14,12 +14,13 @@ let Extends = {
     // http: require("../../plugin/axios")
 };
 // 不挂载到context的白名单
-let whiteList = ["index.js", "model.js", "http.js"];
+let whiteList = ["index.js", "http.js", 'model.js'];
 
+// 
 function loadExtendFiles() {
     let files = fs.readdirSync(__dirname);
     // 过滤掉白名单中的拓展
-    files = files.filter(function(file) {
+    files = files.filter(function (file) {
         if (!whiteList.includes(file)) return file;
     });
     for (let file of files) {
@@ -39,7 +40,7 @@ function contextExtend(app, targetObj) {
 loadExtendFiles();
 
 //对外暴露拓展函数
-module.exports = function(app) {
+module.exports = function (app) {
     for (let target in Extends) {
         contextExtend(app, Extends[target]);
     }
--- src/extend/context/model.js
+++ src/extend/context/model.js
@@ -1,61 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
-function resolve(dir) {
-    return path.join(__dirname, "../../" + dir);
+
+function resolve(...dir) {
+    return path.join(__dirname, "../../", dir);
 }
 
-// 注册数据模型
-function registerModels(modulesPath) {
-    let startTime = new Date();
-    let basePath = resolve(modulesPath);
-    let modules = fs.readdirSync(basePath);
-    for (let model_name of modules) {
-        let dirPath = basePath + "/" + model_name;
-        files = fs.readdirSync(dirPath);
-        //过滤取出所有的模块的对象模型
-        let model_js_files = files.filter(f => {
-            return f.endsWith("model.js");
-        });
-        // console.log("model", model_js_files);
-        for (let js_file of model_js_files) {
-            // let modelName = js_file.replace(".model.js", "").trim();
-            let schema = require(dirPath + "/" + js_file);
-            mongoose.model(model_name, schema);
-        }
-    }
-    console.warn("spend", new Date() - startTime + "ms", "for load models");
+
+
+function loadModel(modelName) {
+    const targetPath = path.resolve(process.cwd(), 'src/modules');
+    const modelPath = path.resolve(`${targetPath}/${modelName}`, `${modelName}.model.js`)
+    return require(modelPath);
 }
 
-// 加载目标文件夹下指定类型的文件
-function loadTargetfiles(modulesPath, extname) {
-    let startTime = new Date();
-    let basePath = resolve(modulesPath);
-    let modules = fs.readdirSync(basePath);
-    for (let model_name of modules) {
-        let dirPath = basePath + "/" + model_name;
-        files = fs.readdirSync(dirPath);
-        //过滤取出所有的模块的对象模型
-        let model_js_files = files.filter(f => {
-            return f.endsWith(extname);
-        });
-        for (let js_file of model_js_files) {
-            let targetPath = path.join(dirPath, "/", js_file);
-            console.log("targetPath", targetPath);
-            require(targetPath);
-        }
-    }
-    console.warn("spend", new Date() - startTime + "ms", "for load models");
-}
 
-//执行模型注入
-// registerModels("modules");
-
-// 加载模型文件
-// loadTargetfiles("modules", "model.js");
 
 module.exports = {
-    findModel: function(modelName) {
-        return mongoose.model(modelName);
+    findModel: function (modelName) {
+        return loadModel(modelName);
     }
 };
