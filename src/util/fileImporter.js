d5df051d29ade077ee61ffee1ecda7df765efe81
VALR <641749563@qq.com>
2019-07-20 20:21:30 +0800

    新增文件导入器方法

---
+++ src/util/fileImporter.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+global.importModel = function (modelName) {
+    return require(path.resolve(process.cwd(), `src/models/${modelName}.js`))
+}
+
+global.importSrc = function (filePath) {
+    return require(path.resolve(process.cwd(), `src/${filePath}`))
+}
+
+global.importPlugin = function (pluginName) {
+    return require(path.resolve(process.cwd(), `src/plugin/${pluginName}`))
+}
+
+global.importMiddlewares = function (pluginName) {
+    return require(path.resolve(process.cwd(), `src/middlewares/${pluginName}`))
+}
